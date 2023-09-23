5ee8a46310e43a0f1c0f8f49a035ddfde062853f
qawaki <38139905+qawaki@users.noreply.github.com>
2023-09-23 03:28:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def creds_entered():
 
 def authenticate_user():
     if "authenticated" not in st.session_state:
-        st.text_input(label="Username:", value=" ", key="user", on_change=creds_entered)
-        st.text_input(label="Password:", value=" ", key="passwd", type="password", on_change=creds_entered)
+        st.text_input(label="Username:", value="", key="user", on_change=creds_entered)
+        st.text_input(label="Password:", value="", key="passwd", type="password", on_change=creds_entered)
         return False
     else:
         if st.session_state["authenticated"]:
