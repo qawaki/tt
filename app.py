06bcd341dab6514a3ed99d8d2290830e1a175f7a
qawaki <38139905+qawaki@users.noreply.github.com>
2023-09-23 01:56:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,7 @@ usernames = ["russcopley", "kyleovens"]
 file_path = Path("./hashed_pw.pkl")
 with file_path.open("rb") as file:
     hashed_passwords = pickle.load(file)
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
-                                    "sales_dashboard", "abcdef", cookie_expiry_days=30)
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "sales_dashboard", "abcdef", cookie_expiry_days=30)
 name, authentication_status, username = authenticator.login("Login", "main")
 
 if authentication_status == False:
