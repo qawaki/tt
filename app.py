9d6d00856b7b9941d0e2a0bd90097c85f0b5e176
qawaki <38139905+qawaki@users.noreply.github.com>
2023-09-23 03:24:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -19,32 +19,32 @@ st.set_page_config(
     layout="wide"
 )
 def creds_entered():
-        if st.session_state["user"].strip() == "admin" and st.session_state["passwd"].strip() == "admin":
-              st.session_state["authenticated"] = True
+    if st.session_state["user"].strip() == "admin" and st.session_state["passwd"].strip() == "admin":
+        st.session_state["authenticated"] = True
+    else:
+        st.session_state["authenticated"] = False
+        
+        if not st.session_state["passwd"]:
+            st.warning("Please enter your password.")
+        elif not st.session_state["user"]:
+            st.warning("Please enter your username.")
         else:
-                    
-                st.session_state["authenticated"] = False
-                
-                if not st.session_state["passwd"]:
-                        st.warning("Please enter your password.")
-                elif not st.session_state["user"]:
-                        st.warning("Please enter your username.")
-                else:
-                        st.error("Invalid Username/Password")
+            st.error("Invalid Username/Password")
 
 
 def authenticate_user():
-        if "authenticated" not in st.session_state:
-                st.text_input(label="Username: ", value = " ", key = "user", on_change = creds_entered)
-                st.text_input(label="Password: ", value = " ", key = "passwd", type = "password", on_change = creds_entered)
-                return False
-        else:   
-                if st.session_state["authenticated"]:
-                        return True
-                else:
-                        st.text_input(label="Username: ", value = " ", key = "user", on_change = creds_entered)
-                        st.text_input(label="Password: ", value = " ", key = "passwd", type = "password", on_change = creds_entered)
-                        return False
+    if "authenticated" not in st.session_state:
+        st.text_input(label="Username:", value=" ", key="user", on_change=creds_entered)
+        st.text_input(label="Password:", value=" ", key="passwd", type="password", on_change=creds_entered)
+        return False
+    else:
+        if st.session_state["authenticated"]:
+            return True
+        else:
+            st.text_input(label="Username:", value=" ", key="user", on_change=creds_entered)
+            st.text_input(label="Password:", value=" ", key="passwd", type="password", on_change=creds_entered)
+            return False
+
 if authenticate_user():
         def fetch_csv_from_url(url):
             response = requests.get(url)
